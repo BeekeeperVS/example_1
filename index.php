8dfa56a9a7c19f568cf8116a333dbcebb42050df
Vitalii <beekeepervitaliy@gmail.com>
2020-05-01 23:52:55 +0300

    version 1.0.0

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+//use src\Hello;
+use bortik\test\example\Hello;
+require __DIR__  ."/vendor/autoload.php";
+
+new Hello();
---
+++ src/Hello.php
@@ -0,0 +1,11 @@
+<?php
+namespace bortik\test\example;
+
+
+class Hello
+{
+    public function __construct()
+    {
+        echo 'Hello word!!!';
+    }
+}
